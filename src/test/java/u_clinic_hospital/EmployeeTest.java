2670dbc9721eefb454d70d14e19afa729b650496
Brian Bross <brian.bross@gmail.com>
2019-09-17 21:29:32 -0400

    Flushed out employee class with tests around attributes

--- src/test/java/u_clinic_hospital/EmployeeTest.java
+++ src/test/java/u_clinic_hospital/EmployeeTest.java
@@ -1,5 +1,23 @@
 package u_clinic_hospital;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
 public class EmployeeTest {
 
+	@Test
+	public void employeeShouldHaveName() throws Exception {
+		Employee underTest = new Employee("Test Employee");
+		String result = underTest.getName();
+		assertThat(result, is("Test Employee"));
+	}
+	
+	@Test
+	public void shouldBeAbleToPaySalaries() throws Exception {
+		Employee underTest = new Employee("Test Employee");
+		underTest.paySalary();
+		assertThat(underTest.getHasBeenPaid(), is(true));
+	}
 }
