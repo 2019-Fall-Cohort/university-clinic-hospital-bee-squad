6b66be684155cb4a77cbba04daa2ea5c1bde686f
Brian Bross <brian.bross@gmail.com>
2019-09-17 21:36:29 -0400

    corrected typo, emp class and tests

--- src/test/java/u_clinic_hospital/EmployeeTest.java
+++ src/test/java/u_clinic_hospital/EmployeeTest.java
@@ -15,9 +15,25 @@ public class EmployeeTest {
 	}
 	
 	@Test
+	public void employeeShouldHaveNumberSalaryAndPaid() throws Exception {
+		Employee underTest = new Employee("Doesnt Need A Name For This Test");
+		underTest.setEmpNumber(110);
+		underTest.setSalary(110000);
+		
+		int testEmpNumber = underTest.getEmpNumber();
+		int testEmpSalary = underTest.getSalary();
+		boolean testBeenPaid = underTest.getHasBeenPaid();
+		
+		assertThat(testEmpNumber, is(110));
+		assertThat(testEmpSalary, is(110000));
+		assertThat(testBeenPaid, is(false));
+	}
+	
+	@Test
 	public void shouldBeAbleToPaySalaries() throws Exception {
 		Employee underTest = new Employee("Test Employee");
 		underTest.paySalary();
-		assertThat(underTest.getHasBeenPaid(), is(true));
+		boolean testHasBeenPaid = underTest.getHasBeenPaid();
+		assertThat(testHasBeenPaid, is(true));
 	}
 }
