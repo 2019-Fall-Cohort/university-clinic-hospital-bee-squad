d3b1599eb7d4f7cc5ea4c95d12f8f6eaa520bf7f
BarbaraTaylor <btaylor1787@gmail.com>
2019-09-19 12:50:12 -0400

    Created Nurse class, extends Employee class

---
+++ src/main/java/u_clinic_hospital/Nurse.java
@@ -0,0 +1,11 @@
+package u_clinic_hospital;
+
+public class Nurse extends Employee {
+
+	public Nurse(String name) {
+		super(name);
+	}
+	
+	
+
+}
---
+++ src/test/java/u_clinic_hospital/NurseTest.java
@@ -0,0 +1,11 @@
+package u_clinic_hospital;
+
+public class NurseTest {
+	
+	@Test 
+	public void shouldCreateNewNurse() {
+		Employee newNurse = new Nurse("nurseName");
+	}
+
+
+}
