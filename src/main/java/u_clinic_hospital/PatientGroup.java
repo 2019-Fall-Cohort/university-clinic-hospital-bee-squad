48463d5dcab813964671d7733f94442cb4dd6599
Brian Bross <brian.bross@gmail.com>
2019-09-22 21:35:55 -0400

    refactored adding patients feature

--- src/main/java/u_clinic_hospital/PatientGroup.java
+++ src/main/java/u_clinic_hospital/PatientGroup.java
@@ -4,13 +4,19 @@ import java.util.Collection;
 import java.util.HashMap;
 
 public class PatientGroup {
-	
+
 	HashMap<String, Patient> patientList;
 
 	public PatientGroup() {
 		patientList = new HashMap<>();
+		Patient jessika = new Patient("Jessika");
+		patientList.put("Jessika", jessika);
+		Patient nathan = new Patient("Nathan");
+		patientList.put("Nathan", nathan);
+		Patient je$$iqua = new Patient("Je$$iqua");
+		patientList.put("Je$$iqua", je$$iqua);
 	}
-	
+
 	public void addPatientToGroup(Patient testPatient) {
 		patientList.put(testPatient.getName(), testPatient);
 	}
@@ -21,6 +27,15 @@ public class PatientGroup {
 
 	public Patient retrievePatient(String patientName) {
 		return patientList.get(patientName);
+	}
+
+	public void printPatientAttributes() {
+		String arrayRow = "";
+		for (Patient thisOne : patientList.values()) {
+			arrayRow = (String.format("|%-17s", thisOne.getName())) + (String.format("|%-12d", thisOne.getBloodLevel()))
+					+ (String.format("|%-12d|", thisOne.getHealthLevel()));
+			System.out.println(arrayRow);
+		}
 	}
 
 }
